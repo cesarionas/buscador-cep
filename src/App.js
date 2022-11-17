595a3566b43e81a3e45bc44c5f1efd46a7cb6416
ITERIS\vinicius.cesario <vinicius.cesario@iteris.com.br>
2022-11-17 14:59:56 -0300

    buscador cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import api from "./service/api";
 
 function App() {
+  const [cep, setCep] = useState("");
+  const [search, setSearch] = useState({});
+  async function findCep() {
+    if (cep === "") {
+      alert("Digite um cep!");
+      return;
+    }
+    try {
+      const response = await api.get(`${cep}/json`);
+      setSearch(response.data);
+      setCep("");
+    } catch {
+      alert("Ops! Erro a buscar!");
+      setCep("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Cep:
+        <input
+          type="text"
+          className="input"
+          value={cep}
+          onChange={(e) => {
+            setCep(e.target.value);
+          }}
+        ></input>
+      </label>
+      <button onClick={findCep}>Pesquisar</button>
+      {Object.keys(search).length > 0 && (
+        <div>
+          <h1>Cep: {search.cep}</h1>
+          <span>Rua: {search.logradouro}</span>
+          <br />
+          <span> Bairro: {search.bairro}</span>
+          <br />
+          <span>
+            Cidade: {search.localidade} - {search.uf}
+          </span>
+          <br />
+        </div>
+      )}
     </div>
   );
 }
